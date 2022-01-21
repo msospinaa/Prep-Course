b7b48a0024693dec3095272d85893075adcfa5a3
msospinaa <msospinaa@unal.edu.co>
2022-01-21 14:54:11 -0500

    Homework 03-JS-|| 21/02/2021 14:54

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -154,12 +154,20 @@ function operadoresLogicos(num1, num2, num3) {
     ){
     return "Número 1 es mayor y positivo";
   } else if (
-    num1 < 0 ||
-    num2 < 0 ||
-    num3 < 0 
+    math.sign (num1) === -0 ||
+    math.sign (num1) === -1 ||
+    math.sign (num2) === -0 ||
+    math.sign (num2) === -1 ||
+    math.sign (num3) === -0 ||
+    math.sign (num3) === -1 
   ){
-    return "Hay negativos"
-  } 
+    return "Hay negativos";
+  } else if (
+    num3 >> num1 ||
+    num3 >> num2 
+  ) {
+    return num3 + 1;
+  }
 }
 
 function esPrimo(numero) {
