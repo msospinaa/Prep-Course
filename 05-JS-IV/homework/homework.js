740c00890aa182193f6b9122412f694b8564cf10
msospinaa <msospinaa@unal.edu.co>
2022-01-22 22:32:05 -0500

    HW 05-JS-IV 22/01/2021

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -6,6 +6,14 @@ function crearGato(nombre, edad) {
   // Agrega un método (funcion) llamado "meow" que devuelva el string "Meow!"
   // Devuelve el objeto
   // Tu código:
+  const gato = { 
+    nombre: nombre, 
+    edad: edad, 
+    meow: function (){
+    return "Meow!"
+  }
+}
+  return gato;
 }
 
 
@@ -14,6 +22,9 @@ function agregarPropiedad(objeto, property) {
   // Devuelve el objeto
   // NOTA: El nombre de la propiedad no es "propiedad", el nombre es el valor del argumento llamado "property" (una cadena/string)
   // Tu código:
+  var objeto = {objeto: property, property: null
+ } 
+  return objeto
 }
 
 function invocarMetodo(objeto, metodo) {
@@ -21,6 +32,7 @@ function invocarMetodo(objeto, metodo) {
   // Invoca ese método
   // Nada necesita ser devuelto ("returned")
   // Tu código:
+  
 }
 
 function multiplicarNumeroDesconocidoPorCinco(objetoMisterioso) {
